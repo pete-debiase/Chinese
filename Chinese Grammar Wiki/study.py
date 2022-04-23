144c738c0256a9eb5aa1cd2f55234970c4786ae1
Pete DeBiase <pete@debiase.io>
2022-04-23 09:50:38 -0700

    Add feature for auto-scraping example sentences

--- Chinese Grammar Wiki/study.py
+++ Chinese Grammar Wiki/study.py
@@ -4,7 +4,10 @@
 from collections import defaultdict
 import json
 import random
+import requests
 import os
+
+from bs4 import BeautifulSoup
 
 # ┌─────────────────────────────────────────────────────────────────────────────
 # │ Basic setup
@@ -49,10 +52,11 @@ with open('studied.json', 'w+', newline='\n', encoding='utf-8') as f:
     json.dump(studied_points, f, indent=2)
 
 print(f"Chinese Grammar Wiki\n{lesson['level']}: {lesson['title']}\n")
-print(f'''<a href="{lesson['url']}">{lesson['level']}: {lesson['title']}</a>\n\n''')
+link = f'''<a href="{lesson['url']}">{lesson['level']}: {lesson['title']}</a>'''
+print(link, '\n\n')
 
 # ┌─────────────────────────────────────────────────────────────────────────────
-# │ Display stats
+# │ Display overall progress stats
 # └─────────────────────────────────────────────────────────────────────────────
 scraped_points_by_level = organize_by_level(scraped_points)
 studied_points_by_level = organize_by_level(studied_points)
@@ -63,3 +67,37 @@ for level in levels:
     num_scraped = len(scraped_points_by_level[level])
     ratio = num_studied / num_scraped
     print(f"{level}: {num_studied} / {num_scraped} ({ratio:.0%})")
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Pull example sentences from selected point
+# └─────────────────────────────────────────────────────────────────────────────
+url = lesson['url']
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+example_groups = soup.find_all('div', {'class': 'liju'})
+
+examples = []
+for group in example_groups:
+    items = group.find_all('li')
+    for i in items:
+        chinese = i.text
+        try:
+            expl = i.find_all('span', {'class': 'expl'})
+            pinyin = i.find_all('span', {'class': 'pinyin'})[0].text
+            english = i.find_all('span', {'class': 'trans'})[0].text
+            if expl: expl = expl[0].text
+
+            if expl: chinese = chinese.replace(expl, '')
+            for rep in [pinyin, english, ' ']:
+                chinese = chinese.replace(rep, '')
+            pinyin.replace('  ', ' ')
+
+            example = f'CGW<br><br>{chinese}\t\t{pinyin.strip()}\t{english.strip()}\t\t\t{link}'
+            example.replace('  ', ' ')
+        except:
+            example = chinese
+        examples.append(example)
+
+examples = [e for e in examples if e]
+with open('examples.tsv', 'w+', newline='\n', encoding='utf-8') as f:
+    f.write('\n'.join(examples))
