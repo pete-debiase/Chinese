e6c18c8d97e717ae9153c6496586cfacd954f302
Pete DeBiase <pete@debiase.io>
2022-04-26 05:16:29 -0700

    Convert numeric pinyin within defs to unicode pinyin

--- CEDICT/update.py
+++ CEDICT/update.py
@@ -18,14 +18,18 @@ with open('cedict_ts.u8', 'r', encoding='utf-8') as f:
 cedict = {}
 for e in entries:
     # CEDICT format: trad simp [pinyin] /def1/def2/.../
-    regex_search = re.search(r'^(.*?) (.*?) \[(.*?)\] /(.*)/', e).groups()
-    trad = regex_search[0]
-    simp = regex_search[1]
-    pinyin = regex_search[2]
-    defs = regex_search[3]
+    parsed = re.search(r'^(.*?) (.*?) \[(.*?)\] /(.*)/$', e).groups()
+    trad = parsed[0]
+    simp = parsed[1]
+    pinyin = parsed[2]
+    defs = parsed[3]
 
     pinyin = decode_pinyin(pinyin)
     defs = defs.replace('/', '; ')
+    inner_pinyins = re.findall(r'\[(.*?)\]', defs)
+    for ip in inner_pinyins:
+        pinyin_unicode = decode_pinyin(ip)
+        defs = defs.replace(ip, pinyin_unicode)
 
     cedict[simp] = {'trad': trad, 'simp': simp, 'pinyin': pinyin, 'defs': defs}
     cedict[trad] = {'trad': trad, 'simp': simp, 'pinyin': pinyin, 'defs': defs}
