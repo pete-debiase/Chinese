31780c48882f11e1b83783908e569d8cfc0f3d6b
Pete DeBiase <pete@debiase.io>
2022-04-27 01:20:44 -0700

    Export trad-only and simp-only text files for jieba

--- CEDICT/update.py
+++ CEDICT/update.py
@@ -7,8 +7,6 @@ import re
 
 from pinyin import decode_pinyin
 
-# TODO: Export trad-only and simp-only .txts for jieba
-
 # ┌─────────────────────────────────────────────────────────────────────────────
 # │ Download latest CEDICT
 # └─────────────────────────────────────────────────────────────────────────────
@@ -20,6 +18,7 @@ with open('cedict_ts.u8', 'r', encoding='utf-8') as f:
     raw_entries = [line.strip() for line in f if line[0] != '#']
 
 cedict = defaultdict(list)
+trad_all, simp_all = [], []
 for raw_entry in raw_entries:
     # CEDICT format: trad simp [pinyin] /def1/def2/.../
     parsed = re.search(r'^(.*?) (.*?) \[(.*?)\] /(.*)/$', raw_entry).groups()
@@ -39,6 +38,18 @@ for raw_entry in raw_entries:
     parsed_entry = {'trad': trad, 'simp': simp, 'pinyin': pinyin, 'defs': defs}
     for key in keys: cedict[key].append(parsed_entry)
 
+    trad_all.append(trad)
+    simp_all.append(simp)
+
 
 with open('cedict_ts.json', 'w+', newline='\n', encoding='utf-8') as f:
     json.dump(cedict, f, indent=2, ensure_ascii=False)
+
+# ┌─────────────────────────────────────────────────────────────────────────────
+# │ Create text files to power Jieba segmentation
+# └─────────────────────────────────────────────────────────────────────────────
+with open('cedict_t_jieba.txt', 'w+', newline='\n', encoding='utf-8') as f:
+    f.write('\n'.join(trad_all))
+
+with open('cedict_s_jieba.txt', 'w+', newline='\n', encoding='utf-8') as f:
+    f.write('\n'.join(simp_all))
